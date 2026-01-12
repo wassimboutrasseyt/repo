8cdb50bd092b07055ffcf80ee7806a582f071ad2
wassimboutrasseyt <boutrasseytwassim@gmail.com>
2026-01-12 07:28:48 +0100

    last

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
 	// Keep the server start log short and useful.
-	console.log(`Quote service listening on http://localhost:${port}`);
+	console.log(`Quote service listening on http://0.0.0.0:${port}`);
 });
